0c400e558ef84860cab12292ba62b117fd6f3f40
choimory <choimory@naver.com>
2021-05-24 20:47:01 +0900

    Initial commit

---
+++ src/main/java/com/practice/helloquerydsl/HelloQuerydslApplication.java
@@ -0,0 +1,13 @@
+package com.practice.helloquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloQuerydslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/helloquerydsl/HelloQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.helloquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloQuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
