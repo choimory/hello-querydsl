651dcc483425b1390701ac88709faefb26515603
choimory <choimory@naver.com>
2021-05-26 21:08:25 +0900

    BoardMapper 작성

--- src/main/java/com/practice/helloquerydsl/board/mapper/BoardMapper.java
+++ src/main/java/com/practice/helloquerydsl/board/mapper/BoardMapper.java
@@ -1,4 +1,21 @@
 package com.practice.helloquerydsl.board.mapper;
 
+import com.practice.helloquerydsl.board.dto.BoardDto;
+import com.practice.helloquerydsl.board.entity.Board;
+import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+@Component
 public interface BoardMapper {
+    BoardDto toDto(Board board);
+    List<BoardDto> toDto(List<Board> boards);
+    List<BoardDto> toDto(Page<Board> boards);
+
+    Board toEntity(BoardDto boardDto);
+    List<Board> toEntity(List<BoardDto> boardDtos);
+    List<Board> toEntity(Page<BoardDto> boardDtos);
 }
