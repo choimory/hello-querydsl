bc2dd1d9b06bc511e80382799a648e4e70a836dd
choimory <choimory@naver.com>
2021-06-07 21:45:03 +0900

    요청주소 매핑

--- src/main/java/com/practice/helloquerydsl/board/BoardController.java
+++ src/main/java/com/practice/helloquerydsl/board/BoardController.java
@@ -4,15 +4,19 @@ import com.practice.helloquerydsl.board.dto.BoardDto;
 import com.practice.helloquerydsl.board.service.BoardService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/board")
 public class BoardController {
     private final BoardService boardService;
 
+    @GetMapping
     public List<BoardDto> getBoards(BoardDto param, Pageable pageable){
         return boardService.getBoardsDynamically(param, pageable);
     }
