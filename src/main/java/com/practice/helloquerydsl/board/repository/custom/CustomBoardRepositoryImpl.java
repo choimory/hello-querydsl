e421d0c00645623bc813ad47024c846d14789923
choimory <choimory@naver.com>
2021-06-07 21:44:53 +0900

    BooleanBuilder를 이용한 로직을 BooleanExpression을 이용한 로직으로 수정

--- src/main/java/com/practice/helloquerydsl/board/repository/custom/CustomBoardRepositoryImpl.java
+++ src/main/java/com/practice/helloquerydsl/board/repository/custom/CustomBoardRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.practice.helloquerydsl.board.repository.custom;
 import com.practice.helloquerydsl.board.entity.Board;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -20,7 +21,8 @@ public class CustomBoardRepositoryImpl implements CustomBoardRepository{
 
     @Override
     public List<Board> getBoardsDynamically(Board param, Pageable pageable, LocalDateTime from, LocalDateTime to) {
-        BooleanBuilder booleanBuilder = getBoardsDynamicallyWhere(param, from, to);
+        //BooleanBuilder 사용 로직
+        /*BooleanBuilder booleanBuilder = getBoardsDynamicallyWhere(param, from, to);
 
         return query.select(Projections.fields(Board.class
                                                 , board.id
@@ -31,28 +33,42 @@ public class CustomBoardRepositoryImpl implements CustomBoardRepository{
                                                 , board.boardComments
                                                 , board.deleteAt
                                                 , board.registDateTime
-                                                , board.updateDateTime)).from(board)
-                                                                        .where(booleanBuilder)
-                                                                        .offset(pageable.getOffset())
-                                                                        .limit(pageable.getPageSize())
-                                                                        .fetch();
+                                                , board.updateDateTime))
+                    .from(board)
+                    .where(booleanBuilder)
+                    .offset(pageable.getOffset())
+                    .limit(pageable.getPageSize())
+                    .fetch();*/
+
+        //------------------------------------
+
+        //BooleanExpression 사용 로직
+        return query.selectFrom(board)
+                    .where(containsTitle(param.getTitle())
+                            , containsContent(param.getContent())
+                            //, eqNickname(param.getUser().getNickname())
+                            , betweenRegistDateTime(from, to))
+                    .offset(pageable.getOffset())
+                    .limit(pageable.getPageSize())
+                    .fetch();
     }
 
+    //Deprecated! BooleanExpression 활용 권장
     private BooleanBuilder getBoardsDynamicallyWhere(Board param, LocalDateTime from, LocalDateTime to){
         BooleanBuilder booleanBuilder = new BooleanBuilder();
 
         //contains title
-        if(StringUtils.hasLength(param.getTitle())){
+        if(StringUtils.hasText(param.getTitle())){
             booleanBuilder.and(board.title.contains(param.getTitle()));
         }
 
         //contains content
-        if(StringUtils.hasLength(param.getContent())){
+        if(StringUtils.hasText(param.getContent())){
             booleanBuilder.and(board.content.contains(param.getContent()));
         }
 
         //eq nickname
-        if(StringUtils.hasLength(param.getUser().getNickname())){
+        if(StringUtils.hasText(param.getUser().getNickname())){
             booleanBuilder.and(board.user.nickname.eq(param.getUser().getNickname()));
         }
 
@@ -66,5 +82,29 @@ public class CustomBoardRepositoryImpl implements CustomBoardRepository{
         }
 
         return booleanBuilder;
+    }
+
+    private BooleanExpression containsTitle(String title){
+        return StringUtils.hasText(title) ? board.title.contains(title) : null;
+    }
+
+    private BooleanExpression containsContent(String content){
+        return StringUtils.hasText(content) ? board.content.contains(content) : null;
+    }
+
+    private BooleanExpression eqNickname(String nickname){
+        return StringUtils.hasText(nickname) ? board.user.nickname.eq(nickname) : null;
+    }
+
+    private BooleanExpression betweenRegistDateTime(LocalDateTime from, LocalDateTime to){
+        if(from==null && to==null){
+            return null;
+        }else if(from!=null && to==null){
+            return board.registDateTime.after(from);
+        }else if(from==null && to!=null){
+            return board.registDateTime.before(to);
+        }else {
+            return board.registDateTime.between(from, to);
+        }
     }
 }
